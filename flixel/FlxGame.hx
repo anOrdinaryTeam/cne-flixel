fe115846216f7efcb9a35cc68944cc5e3d3e25f2
Homura <egzozu.be.bas@gmail.com>
2026-07-03 13:58:52 +0300

    I like Float's

--- flixel/FlxGame.hx
+++ flixel/FlxGame.hx
@@ -79,7 +79,7 @@ class FlxGame extends Sprite
 	/**
 	 * Time in milliseconds that has passed (amount of "ticks" passed) since the game has started.
 	 */
-	public var ticks(default, null):Int = 0;
+	public var ticks(default, null):Float = 0;
 
 	/**
 	 * Enables or disables the filters set via `setFilters()`.
@@ -111,7 +111,7 @@ class FlxGame extends Sprite
 	 * Time stamp of game startup. Needed on JS where `Lib.getTimer()`
 	 * returns time stamp of current date, not the time passed since app start.
 	 */
-	var _startTime:Int = 0;
+	var _startTime:Float = 0;
 
 	/**
 	 * Total number of milliseconds elapsed since last update loop.
@@ -945,7 +945,7 @@ class FlxGame extends Sprite
 		return getTimer() - _startTime;
 	}
 
-	dynamic function getTimer():Int
+	dynamic function getTimer():Float
 	{
 		// expensive, only call if necessary
 		return Lib.getTimer();
